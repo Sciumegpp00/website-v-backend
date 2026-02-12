5f6ded1916e544c717f70383bde1ebc2ee756120
Giuseppe Sciumè <sciumegpp@gmail.com>
2026-02-12 15:20:26 +0100

    Hardcoded samples

--- src/modules/viaggi/viaggi.controller.ts
+++ src/modules/viaggi/viaggi.controller.ts
@@ -22,7 +22,36 @@ export const viaggiController = {
 }
 
 // Per ora usiamo un array in memoria (poi DB)
-let viaggi: Viaggio[] = []
+let viaggi: Viaggio[] = [
+  {
+    id: 1,
+    title: "Roma",
+    lat: 41.9028,
+    lng: 12.4964,
+    status: "visited",
+    description: "Colosseo e carbonara ❤️",
+    date: "2023-06-10"
+  },
+  {
+    id: 2,
+    title: "Parigi",
+    lat: 48.8566,
+    lng: 2.3522,
+    status: "visited",
+    description: "Tour Eiffel al tramonto ✨",
+    date: "2024-04-02"
+  },
+  {
+    id: 3,
+    title: "Tokyo",
+    lat: 35.6762,
+    lng: 139.6503,
+    status: "planned",
+    description: "Prossima avventura ✈️",
+    date: "2026-09-01"
+  }
+]
+
 
 export async function getViaggi() {
   return viaggi
